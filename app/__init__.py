eb26f7ce9e9425a7a696530a2fc387ea681a4629
Megan Kwok <megankwok168@gmail.com>
2026-05-13 11:25:20 -0400

    __init__.py accounts --> need to switch to sql

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,60 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+#login
+@app.route("/", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        userData = mongo.users.find_one({"_id": username}, {"password":1})
+        if userData:
+            if password == userData["password"]:
+                session["username"] = username
+                return redirect(url_for('index'))
+            else:
+                flash("Incorrect password. Try again.")
+        else:
+            flash("Username incorrect or not found. Try again.")
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+
+#createaccount
+@app.route("/createaccount", methods = ['GET', "POST"])
+def set_user():
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if mongo.users.find_one({"_id": username}):
+            flash("Username already taken!")
+            return redirect(url_for('set_user'))
+        mongo.users.insert_one({
+            "_id": username,
+            "password": password
+        })
+        session['username'] = username
+        return redirect(url_for('index'))
+    return render_template('createaccount.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+#main
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    return render_template("index.html")
+
+#handling data
+@app.route('/data')
 
 if __name__ == "__main__":
   app.debug = True
