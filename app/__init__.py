c38d88078e0aac57f176cded9a9d48a190b18732
Megan Kwok <megankwok168@gmail.com>
2026-05-13 11:31:52 -0400

    accounts

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 #login
 @app.route("/", methods=["GET", "POST"])
 def login():
-    if 'username' in session:
+    if 'email' in session:
         return redirect(url_for('index'))
     if request.method == 'POST':
-        username = request.form["username"]
+        email = request.form["email"]
         password = request.form["password"]
-        userData = mongo.users.find_one({"_id": username}, {"password":1})
+        userData = auth(email, password)
         if userData:
             if password == userData["password"]:
-                session["username"] = username
+                session["email"] = email
                 return redirect(url_for('index'))
             else:
                 flash("Incorrect password. Try again.")
         else:
-            flash("Username incorrect or not found. Try again.")
+            flash("Email incorrect or not found. Try again.")
         return redirect(url_for('login'))
     return render_template('login.html')
 
@@ -29,22 +29,23 @@ def set_user():
     if 'username' in session:
         return redirect(url_for('index'))
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        if mongo.users.find_one({"_id": username}):
-            flash("Username already taken!")
+        if user_exists(email):
+            flash("User exists!")
             return redirect(url_for('set_user'))
-        mongo.users.insert_one({
-            "_id": username,
-            "password": password
-        })
+        github = request.form['github']
+        name = request.form['name']
+        is_dojo = request.form['is_dojo']
+        classes = request.form['classes']
+        add_user(password, email, github, name, is_dojo, classes)
         session['username'] = username
         return redirect(url_for('index'))
     return render_template('createaccount.html')
 
 @app.route("/logout")
 def logout():
-    session.pop('username', None)
+    session.pop('email', None)
     return redirect(url_for('login'))
 
 #main
