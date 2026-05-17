b37d88205ae1ff58c4099ba79a3ff88e848969ee
Maya Berchin <mayab97@nycstudents.net>
2026-05-17 16:50:27 -0400

    very broken but working on classes and did a lot so committing

--- app/data.py
+++ app/data.py
@@ -5,23 +5,13 @@ import secrets                      # to generate ids
 
 DB_FILE="data.db"
 
-#=============================[GLOBALS]=============================#
-
-USERS_COLS = ['user_id', 'email', 'github', 'name', 'password_hash', 'is_dojo', 'class_id']
-CLASSES_COLS = ['class_id', 'name', 'teacher_id']
-POSTS_COLS = ['post_id', 'poster_id', 'class_id', 'title', 'body', 'category', 'status', 'created_at', 'updated_at', 'upvotes', 'upvoted_by', 'ping']
-FOLLOWUPS_COLS = ['followup_id', 'poster_id', 'post_id', 'body', 'status', 'is_answer', 'created_at', 'updated_at', 'upvotes', 'upvoted_by', 'ping']
-
-CLASS_ID = 0
-
 #=============================[MAKE=TABLES]=============================#
 
 # users
 def create_users_table():
     command =  """
                 CREATE TABLE IF NOT EXISTS users (
-                    user_id         INTEGER     NOT NULL    PRIMARY KEY     AUTOINCREMENT,
-                    email           TEXT        NOT NULL                    UNIQUE,
+                    email           TEXT        NOT NULL    PRIMARY KEY     UNIQUE,
                     github          TEXT,
                     name            TEXT        NOT NULL,
                     password_hash   TEXT        NOT NULL,
@@ -34,9 +24,9 @@ def create_users_table():
 def create_classes_table():
     command =  """
                 CREATE TABLE IF NOT EXISTS classes (
-                    class_id        INTEGER     NOT NULL    PRIMARY KEY     AUTOINCREMENT,
+                    class_id        TEXT        NOT NULL    PRIMARY KEY,
                     name            TEXT        NOT NULL,
-                    teacher_id      TEXT        NOT NULL
+                    teacher_email   TEXT        NOT NULL
                 )"""
     sqlite(command)
 
@@ -44,9 +34,9 @@ def create_classes_table():
 def create_posts_table():
     command =  """
                 CREATE TABLE IF NOT EXISTS posts (
-                    post_id         INTEGER     NOT NULL    PRIMARY KEY     AUTOINCREMENT,
-                    poster_id       INTEGER     NOT NULL,
-                    class_id        INTEGER     NOT NULL,
+                    post_id         TEXT        NOT NULL    PRIMARY KEY,
+                    poster_email    TEXT        NOT NULL,
+                    class_id        TEXT        NOT NULL,
                     title           TEXT        NOT NULL,
                     body            TEXT        NOT NULL,
                     category        TEXT        NOT NULL,
@@ -63,9 +53,9 @@ def create_posts_table():
 def create_followups_table():
     command =  """
                 CREATE TABLE IF NOT EXISTS posts (
-                    followup_id     INTEGER     NOT NULL    PRIMARY KEY     AUTOINCREMENT,
-                    poster_id       INTEGER     NOT NULL,
-                    post_id         INTEGER     NOT NULL,
+                    followup_id     TEXT        NOT NULL    PRIMARY KEY,
+                    poster_email    TEXT        NOT NULL,
+                    post_id         TEXT        NOT NULL,
                     body            TEXT        NOT NULL,
                     status          TEXT        NOT NULL,
                     is_answer       TEXT        NOT NULL                                            DEFAULT 'no',
@@ -92,17 +82,7 @@ def create_tables():
 
 # returns a list of usernames
 def get_all_users():
-    #data = sqlite_fetchall('SELECT email FROM users')
-    data = get_col_list("users", "email")
-    return clean_list(data)
-
-
-def get_id(email):
-    data = get_field("users", "email", email, user_id)
-    return data
-
-def get_email(id):
-    data = get_field("users", "user_id", id, "email")
+    data = get_col("users", "email")
     return data
 
 
@@ -148,7 +128,7 @@ def add_user(email, password, name, github=''):
 
     # use ? for unsafe/user provided variables
     #sqlite('INSERT INTO users(email, github, name, password_hash, is_dojo, class_id) VALUES (?, ?, ?, ?, ?, ?)', (email, github, name, password, 'no', '',))
-    add_row("users", USERS_COLS, [email, github, name, password, 'no', ''])
+    add_row("users", [email, github, name, password, 'no', ''])
 
     return 'success'
 
@@ -159,8 +139,8 @@ def add_user(email, password, name, github=''):
 
 def get_all_classes():
     #data = sqlite_fetchall('SELECT class_id FROM users')
-    data = get_col_list("users", "class_id")
-    return clean_list(data)
+    data = get_col('classes', 'class_id')
+    return data
 
 # get the classes someone is in
 def get_classes(email):
@@ -169,31 +149,38 @@ def get_classes(email):
     return classes
 
 # get the classes someone teaches 
-def teaches_classes(email):
+def get_teaching_classes(email):
     classes = get_classes(email)
-    teaches = [c for c in classes if c in get_teachers(c)]
+    teaches = [c for c in classes if email in get_teachers(c)]
     return teaches
 
 # get the teachers of a class
 def get_teachers(class_id):
-    teachers_str = get_field('classes', 'class_id', class_id, 'teacher_id')
-    teachers = make_list(classes_str)
+    teachers_str = get_field('classes', 'class_id', class_id, 'teacher_email')
+    teachers = make_list(teachers_str)
     return teachers
 
+def add_teacher(class_id, email):
+    teachers = get_teachers(class_id)
+    teachers += [email]
+    teachers_str = merge_list(teachers)
+    update_row('classes', 'class_id', class_id, 'teacher_email', teachers)
 
-def create_class(teacher_id, class_name):
-    global CLASS_ID
-    #sqlite('INSERT INTO classes(class_id, name, teacher_id) VALUES (?, ?, ?)', (teacher_id, class_name,))
-    add_row('classes', CLASS_COLS, [teacher_id, class_name])
-    CLASS_ID += 1
-    teacher_classes = get_classes(get_email(teacher_id))
-    teacher_classes += f', {CLASS_ID}'
-    update_row('users', 'email', get_email(teacher_id), 'class_id', teacher_classes)
+
+def create_class(teacher_email, class_name):
+    class_id = gen_id()
+    add_row('classes', [class_id, class_name, teacher_email])
+    teacher_classes = get_classes(teacher_email)
+    teacher_classes += [class_id]
+    teacher_classes_str = merge_list(teacher_classes)
+    update_row('users', 'email', teacher_email, 'class_id', teacher_classes_str)
+    return class_id
 
 def join_class(email, class_id):
     classes = get_classes(email)
-    classes += f', {class_id}'
-    update_row('users', 'email', email, 'class_id', classes)
+    classes += [class_id]
+    classes_str = merge_list(classes)
+    update_row('users', 'email', email, 'class_id', classes_str)
 
 
 #=============================[GENERAL=HELPERS]=============================#
@@ -254,7 +241,7 @@ def get_row_list(table, col_name, ID):
     return clean_list_2d(data)
 
 # return a list of all items in a column of the table
-def get_col_list(table, col_name):
+def get_col(table, col_name):
     # no unsafe/user-provided vars here, safe to use f-strings
     data = sqlite_fetchall(f'SELECT {col_name} FROM {table}')
     return clean_list(data)
@@ -265,26 +252,15 @@ def get_col_list(table, col_name):
 #---------[modify]---------#
 
 
-def add_row(table, cols=[], vals):
-    
+def add_row(table, vals):
     # 'INSERT INTO tablename(col0, col1, col2) VALUES (?, ?, ?)', (val0, val1, val2,)
-    
-    command = f'INSERT INTO {table}'
-    
-    if (cols != []):
-        cols_str = str(cols)[1:-1]  # make a string of the list and don't include the '[' or ']'
-        command += f'({cols_str})'
-    
-    q_str = ' VALUES ('
+    command = f'INSERT INTO {table} VALUES ('
     for i in range(len(vals)):
-        q_str += '?,'
-    q_str(len(q_str)-1) = ')'
-    command += q_str
-    
+        command += '?,'
+    command = command[:-1]
+    command += ')'
     vals_tup= tuple(vals)
-    
     sqlite(command, vals_tup)
-
 
 def update_row(table, ID_fieldname, id, col_name, item):
     command = f'UPDATE {table} SET {col_name} = ? WHERE {ID_fieldname} = ?'
@@ -364,5 +340,30 @@ def make_list(str, delim=","):
 #=============================[TESTING]=============================#
 
 if __name__ == "__main__":
+    
     create_tables()
+    
     add_user("mayaberchin@gmail.com", "hello", "Maya Berchin")
+    add_user("other@gmail.com", "other", "Other Student")
+    print(str(get_all_users()))
+    add_user("b@b.com", "b", "b b")
+    
+    print(str(get_all_classes()))
+    class_id = create_class("mayaberchin@gmail.com", "testclass")
+    create_class("b@b.com", "dontjoin")
+    print("\n" + str(get_all_classes()))
+    print("Class teachers: " + str(get_teachers(class_id)))
+    
+    join_class("other@gmail.com", class_id)
+    print("\nClasses Maya is in: " + str(get_classes("mayaberchin@gmail.com")))
+    print("Classes Maya teaches: " + str(get_teaching_classes("mayaberchin@gmaill.com")))
+    print("Classes Other is in: " + str(get_classes("other@gmail.com")))
+    print("Classes Other teaches: " + str(get_teaching_classes("other@gmail.com")))
+    
+    print("\nPromoting Other...")
+    add_teacher(class_id, 'other@gmail.com')
+    print("Class teachers: " + str(get_teachers(class_id)))
+    print("Classes Maya is in: " + str(get_classes("mayaberchin@gmail.com")))
+    print("Classes Maya teaches: " + str(get_teaching_classes("mayaberchin@gmaill.com")))
+    print("Classes Other is in: " + str(get_classes("other@gmail.com")))
+    print("Classes Other teaches: " + str(get_teaching_classes("other@gmail.com")))
