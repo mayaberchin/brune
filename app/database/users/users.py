b7892c5fa2f01a34cbd65f132a478b5ea6bf13dc
Maya Berchin <mayab97@nycstudents.net>
2026-07-24 00:14:11 -0400

    organize helper functions at the bottom of the file

--- app/database/users/users.py
+++ app/database/users/users.py
@@ -151,7 +151,28 @@ def auth(email, password):
 
 
 
-#---------[users-helpers]---------#
+#=====================================================[TABLE-HELPERS]=====================================================#
+
+
+
+                                            #---------[accessors]---------#
+
+
+# PURPOSE
+# These helper functions serve to access data from the 'users' table.
+# They correspond to more general functions in tables.py--these are basically wrappers.
+
+# PARAMETERS
+# email         STRING          The email corresponding to the entry (row) containing data we want to access.
+# field_name    STRING          The name of the field (column) containing the data of interest to us.
+# col_name      STRING          The name of the entire column we are getting from the table.
+
+# RETURN VALUES
+# get_users_field() returns the first piece of data from 'users' that matches the criteria.
+# get_users_field_list() returns a list of all data from 'users' that matches the criteria.
+# get_users_row() returns the first row from 'users' that matches the criteria, formatted as a list.
+# get_users_row_list() returns a list of all rows from 'users' that match the criteria, formatted as a 2d list.
+# get_users_col() returns data from an entire column of 'users' formatted as a list.
 
 
 def get_users_field(email, field_name):
@@ -160,23 +181,39 @@ def get_users_field(email, field_name):
 def get_users_field_list(email, field_name):
     return helpers.get_field_list('users', 'email', email, field_name)
 
-
 def get_users_row(email):
     return tables.get_row('users', 'email', email)
 
 def get_users_row_list(email):
     return tables.get_row_list('users', 'email', email)
 
-
 def get_users_col(col_name):
     return tables.get_col('users', col_name)
+
+
+
+
+                                            #---------[modifiers]---------#
+
+
+# PURPOSE
+# These helper functions serve to modify data in the 'users' table.
+# They correspond to more general functions in tables.py--these are basically wrappers.
+
+# PARAMETERS
+# email         STRING                          The email corresponding to the entry (row) containing data we want to modify.
+# col_name      STRING                          The name of the field (column) containing data we want to modify.
+# value(s)      STRING or LIST-STRING           The new data we want to put in the table (add or update).
+
+# RETURN VALUES
+# None of these functions return anything.
 
 
 def add_users_row(values):
     tables.add_row('users', values)
 
-def update_users_row(email, col_name, col_val):
-    tables.update_row('users', 'email', email, col_name, col_val)
+def update_users_row(email, col_name, value):
+    tables.update_row('users', 'email', email, col_name, value)
 
 def delete_users_row(email):
     tables.delete_row('users', 'email', email)
